a7a5e13be64d1f3ae826bd90801433b28b394698
Kerby Ferris <kerbyferris@gmail.com>
2017-01-05 23:44:17 -0800

    handle level click

--- demo/demo.js
+++ demo/demo.js
@@ -20,46 +20,52 @@ export default class Demo extends Component {
     super(props);
 
     this.state = {
-      playing: true,
+      playing: false,
       tempo: 110,
       defcon: 1,
       maxReading: 21,
       reading: 0,
+      dataOverride: true,
     };
 
     this.handleAudioProcess = this.handleAudioProcess.bind(this);
     this.handlePlayToggle = this.handlePlayToggle.bind(this);
+    this.handleLevelSelect = this.handleLevelSelect.bind(this);
   }
 
   handleAudioProcess(analyser) {
     this.visualization.audioProcess(analyser);
   }
 
-  convertToDefcon(value) {
+  convertToDefcon = value => {
+    const maxValue = (this.state.maxReading < value)? this.state.maxReading : value
     const divisor = 5 //convert values into defcon levels 1-5
 
-    return (Math.floor(parseFloat(value/divisor)) + 1)
+    return (Math.floor(maxValue/divisor)+1)
   }
   
   componentDidMount() {
-    setInterval( () => { 
-      fetch('./data.json')
-        .then((res) => res.json())
-        .then((data) => {
-          let d = data[1] 
-          let defcon = this.convertToDefcon(d)
+    if(!this.state.dataOverride) {
+      setInterval( () => {
+        fetch('./data.json')
+          .then((res) => res.json())
+          .then((data) => {
+            let d = data[1]
+            let defcon = this.convertToDefcon(d)
 
-          this.setState({
-            defcon: defcon})
-          this.setState({reading: d})
-        });
-    }, 1000 );
+            this.setState({defcon: defcon})
+            this.setState({reading: d})
+          });
+      }, 1000 );
+    }
   }
 
   handlePlayToggle() {
-    this.setState({
-      playing: !this.state.playing,
-    });
+    this.setState({playing: !this.state.playing});
+  }
+
+  handleLevelSelect(e) {
+    this.setState({defcon: e});
   }
 
   renderDefconFive() {
@@ -140,13 +146,8 @@ export default class Demo extends Component {
   }
 
   render() {
-    const defconOne = (this.state.defcon <= 1);
-    const defconTwo = (this.state.defcon <= 2);
-    const defconThree = (this.state.defcon <= 3);
-    const defconFour = (this.state.defcon <= 4);
-    const defconFive = (this.state.defcon <= 5);
     return (
-      <div>
+      <div id="main">
         <Song
           playing={this.state.playing}
           tempo={this.state.tempo}
@@ -156,28 +157,39 @@ export default class Demo extends Component {
               resolution={16}
               bars={2}
             >
-              {defconOne? this.renderDefconOne() : null}
-              {defconTwo? this.renderDefconTwo() : null}
-              {defconThree? this.renderDefconThree() : null}
-              {defconFour? this.renderDefconFour() : null}
-              {defconFive? this.renderDefconFive() : null}
+              {this.state.defcon <= 1? this.renderDefconOne() : null}
+              {this.state.defcon <= 2? this.renderDefconTwo() : null}
+              {this.state.defcon <= 3? this.renderDefconThree() : null}
+              {this.state.defcon <= 4? this.renderDefconFour() : null}
+              {this.state.defcon <= 5? this.renderDefconFive() : null}
+
             </Sequencer>
           </Analyser>
         </Song>
 
-        <Visualization ref={(c) => { this.visualization = c; }} />
-
+        <div id="visualize">
+          <div className="react-music-metadata">
+            <span>Defcon: {this.state.defcon.toString()}</span>
+            <span>Data In: {this.state.reading.toString()}</span>
+          </div>
+          <Visualization ref={(c) => { this.visualization = c; }} />
+        </div>
+        <div id="meter">
+          <div id="defcon-1" className="defcon" onClick={()=>this.handleLevelSelect(1)}>1</div>
+          <div id="defcon-2" className="defcon" onClick={()=>this.handleLevelSelect(2)}>2</div>
+          <div id="defcon-3" className="defcon" onClick={()=>this.handleLevelSelect(3)}>3</div>
+          <div id="defcon-4" className="defcon" onClick={()=>this.handleLevelSelect(4)}>4</div>
+          <div id="defcon-5" className="defcon" onClick={()=>this.handleLevelSelect(5)}>5</div>
+        </div>
         <button
           className="react-music-button"
           type="button"
+          style={{marginRight:(5 - this.state.defcon) * 160 + 8}}
           onClick={this.handlePlayToggle}
         >
-          {this.state.playing ? 'Stop' : 'Play'}
+          {this.state.playing ? '\u25A0' : '\u25B2'}
         </button>
-        <div className="react-music-button">
-          <p>Defcon: {this.state.defcon.toString()}</p>
-          <p>Data In: {this.state.reading.toString()}</p>
-        </div>
+
       </div>
     );
   }
